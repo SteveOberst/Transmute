4e6111810fcaebdd56f94d5cdbb3cf53ce70179f
Steve Oberst <baumgrau69@gmail.com>
2026-02-17 08:29:02 +0100

    fix(build): move important notation to top of the file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
+import dev.transmute.build.ProjectVersion
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
 }
-
-import dev.transmute.build.ProjectVersion
 
 group = "com.github.SteveOberst.Transmute"
 version = ProjectVersion.resolve(rootDir) // x-release-please-version
